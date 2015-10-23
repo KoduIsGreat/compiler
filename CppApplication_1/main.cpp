d63d2c5d9452e5f53a62bc121c66e171f6b7bfd8
Adam <aashelt90@gmail.com>
2015-10-23 00:45:22 -0400

    initial commit 

---
+++ CppApplication_1/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: Adam
+ *
+ * Created on October 23, 2015, 12:37 AM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
