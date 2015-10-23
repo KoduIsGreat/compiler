df576538831a3406807ded2905d731569aee831c
Adam <aashelt90@gmail.com>
2015-10-23 02:55:24 -0400

    changes to file write

--- CppApplication_1/main.cpp
+++ CppApplication_1/main.cpp
@@ -14,10 +14,13 @@ using namespace std;
 /*
  * 
  */
+const int max_val = 100;
 int main() {
     std::ofstream ofs;
     ofs.open ("test.txt", std::ofstream::out | std::ofstream::app);
-    ofs << " more lorem ipsum";
+    for(int i = 0 ; i<max_val; i++){
+        ofs << rand() % 100+i "\n";
+    }
     ofs.close();
     return 0;
 }
