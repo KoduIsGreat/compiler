9c8a0dd481ce6f0a96ab7daace5a2168e56acd4e
Adam <aashelt90@gmail.com>
2015-10-24 12:50:06 -0400

    accepts file name, file format improved

--- CppApplication_1/main.cpp
+++ CppApplication_1/main.cpp
@@ -8,19 +8,29 @@
 #include <cstdlib>
 #include <iostream>
 #include <fstream>
-
+#include <time.h>
+#include <String>
 using namespace std;
 
 /*
- * 
+ * simple executable to generate a file with 100 random numbers;
  */
 const int max_val = 100;
 int main() {
+    
+    char * filename;
+    cout << "name of file to generate?";
+    cin >> filename;
+    
     std::ofstream ofs;
-    ofs.open ("test.txt", std::ofstream::out | std::ofstream::app);
+    ofs.open (filename, std::ofstream::out | std::ofstream::app);
+    srand(time(NULL));
+    
     for(int i = 0 ; i<max_val; i++){
-        ofs << rand() % 100+i "\n";
+        ofs << rand() % 100+i;
+        ofs << '\n';
     }
+    
     ofs.close();
     return 0;
 }
