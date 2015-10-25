1e79e423408e6e37c0aad1b4a73835451f0868f6
Adam <aashelt90@gmail.com>
2015-10-25 19:05:45 -0400

    included added while loop + comamnds

--- CppApplication_1/main.cpp
+++ CppApplication_1/main.cpp
@@ -11,22 +11,48 @@
 #include <time.h>
 #include <String>
 #include <write.h>
+
 using namespace std;
 
 /*
  * simple executable to generate a file with 100 random numbers;
  */
 void write (char* filename);
-
+int* read(std::string filename);
+int* reverse(int list[]);
 
 int main() {
-    
-    string filename;
-    cout << "name of file to generate?\n";
-    cin >> filename;
-    
-    write(filename);
-    
+    while(true){
+        string command;
+        cout << "commands\n";
+        cout << "read or write\n";
+        cout << "q to quit\n";
+        cout << "are you reading or writing?\n";
+        cin>> command;
+        if(command.compare("read")==0){
+            string filename;
+            cout << "name of file to read\n";
+            cin >> filename;
+            int* rList= reverse(read(filename));
+            for(int i =0 ; i < 100;i++){
+                cout << rList[i];
+                cout <<"\n";
+            }
+        }
+        else if(command.compare("write")==0){
+            string filename;
+            cout << "name of file to generate?\n";
+            cin >> filename;
+
+            write(filename);
+        }
+        else if(command.compare("q")==0){
+            break;
+        }
+        else{
+            cout << "incorrect option";
+        }
+    }
     return 0;
 }
 
